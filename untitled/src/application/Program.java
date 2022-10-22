00ddc591cb6baf78b14b6e14bbb0158671d01ee2
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-10-22 11:49:43 -0300

    Bad solution

--- untitled/src/application/Program.java
+++ untitled/src/application/Program.java
@@ -34,13 +34,10 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkout = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkin.before(now) || checkout.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-            } else if (!checkout.after(checkin)) {
-                System.out.println("Error in reservation: Check-out date must be after check-in date.");
+            String error = reservation.updateDates(checkin, checkout);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
             } else {
-                reservation.updateDates(checkin, checkout);
                 System.out.println("Reservation: " + reservation);
             }
 
@@ -49,5 +46,4 @@ public class Program {
         sc.close();
 
     }
-
 }
